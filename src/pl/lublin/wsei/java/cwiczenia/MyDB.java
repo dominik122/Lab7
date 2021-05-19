57bc2989eb4abe41c5a6ce2e6a39a18644b9e8ac
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-05-19 11:31:11 +0200

    Utworzenie klasy MyDB

---
+++ src/pl/lublin/wsei/java/cwiczenia/MyDB.java
@@ -0,0 +1,62 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MyDB {
+    private String host;
+    private String port;
+    private String dbName;
+    private String user;
+    private String password;
+
+    private Connection conn = null;
+
+    public MyDB(String host, String port, String dbName) {
+        this.host = host;
+        this.port = port;
+        this.dbName = dbName;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private void connect(){
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+        connectionProps.put("serverTimeZone", "Europe/Warsaw");
+
+        String jdbcString = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+        try {
+            conn = DriverManager.getConnection(
+                    jdbcString, connectionProps);
+        } catch (SQLException e){
+            System.out.println("Błąd podłączenia do bazy " + dbName);
+            System.out.println("Komunikat błędu: " + e.getMessage());
+
+            conn = null;
+        }
+        System.out.println("Connected to database");
+    }
+
+    public Connection getConnection(){
+        if(conn == null) connect();
+        return conn;
+    }
+}
