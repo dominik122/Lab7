e0a9b3a959f1fcd287946dc980cb4948f2eae4f9
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-05-19 11:43:47 +0200

    Test klasy MyDB

--- src/pl/lublin/wsei/java/cwiczenia/MyDB.java
+++ src/pl/lublin/wsei/java/cwiczenia/MyDB.java
@@ -7,14 +7,14 @@ import java.util.Properties;
 
 public class MyDB {
     private String host;
-    private String port;
+    private Number port;
     private String dbName;
     private String user;
     private String password;
 
     private Connection conn = null;
 
-    public MyDB(String host, String port, String dbName) {
+    public MyDB(String host, Number port, String dbName) {
         this.host = host;
         this.port = port;
         this.dbName = dbName;
@@ -58,5 +58,16 @@ public class MyDB {
     public Connection getConnection(){
         if(conn == null) connect();
         return conn;
+    }
+    
+    public void closeConnection(){
+        if(conn != null){
+            try{
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Błąd przy zamykaniu połączenia bazdanowego: " + e.getMessage());
+            }
+        }
+        conn = null;
     }
 }
---
+++ src/pl/lublin/wsei/java/cwiczenia/test/TestMyDB.java
@@ -0,0 +1,20 @@
+package pl.lublin.wsei.java.cwiczenia.test;
+
+import pl.lublin.wsei.java.cwiczenia.MyDB;
+
+import java.sql.Connection;
+
+public class TestMyDB {
+    public static void main(String[] args){
+        MyDB db = new MyDB("localhost", 3306, "mydb");
+        db.setUser("root");
+        db.setPassword("maciek123");
+        Connection conn = db.getConnection();
+
+        if (conn != null){
+            System.out.println("Połączenie nawiązane");
+        }
+        db.closeConnection();
+        System.out.println("Połączenie z bazą zakończone");
+    }
+}
